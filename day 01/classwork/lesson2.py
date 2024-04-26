39825131a3450445313f865db3175d370e6cf4c8
lilecxvaradze <lilecxvaradze@gmail.com>
2024-04-26 22:13:19 +0100

    yee

---
+++ day 01/classwork/lesson2.py
@@ -0,0 +1,28 @@
+name = 'lile'
+# name     არის ცვლადი
+# = არის ცვლადისთვის მნიშვლენოვანი მინიჭებული სიმბოლო
+# 'lile'  არის ცვლადისთვის მნიშვნელობა
+
+surename = 'tskhvaradze'
+
+# print(name) 
+#პრინტ პუნქციას გადაეცემა ეკრანზე გამოსატანი ცვლადი ან სტრინგი=ობიექტში
+
+
+name = 'lile' # ეს არის str (string) ტიპის ცვლადი
+age = 13  #ეს არის int (integer) მთელი რიცხვი
+height = 152.5   #ეს არის float ტიპის ცვლადი (ათწილად)
+#boolean (bool) ტიპის ცვლადი
+
+knowa_programming = True   #True or False
+is_ugly = False  #snakecase (უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False    #ჯავასკრიპთული camelcase
+
+print(name + ' ' + surename)
+
+#print(name + age)
+
+print(type(age))    #შემოწმება იმის თყ ის int ან str არის
+
+
---
+++ day 01/homework/homework.py
@@ -0,0 +1,10 @@
+name = "me var lile"
+
+surname = "tskhvaradze,"
+
+age = "13 wlis"
+
+height = "da 152.5 simagleshi"
+
+
+print(name + ' ' + surname + ' ' + age + ' ' + height)

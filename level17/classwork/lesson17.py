87d3c9cb348c0ffea72249664df71f2e1b10cb7f
lilecxvaradze <lilecxvaradze@gmail.com>
2024-06-21 17:41:09 +0100

    commit

---
+++ level17/classwork/lesson17.py
@@ -0,0 +1,38 @@
+#num = []
+
+#for i in range(1,10):
+#    num.append(i)
+
+#print(num)
+#print(len(num))
+
+
+#num1 = int(input("please enter first number: "))
+#num2 = int(input("please enter second number: "))
+
+#nums = []
+
+#for i in range(num1, num2):
+#    nums.append(i)
+
+#print(min(nums))
+#print(max(nums))
+#print(sum(nums))
+
+
+
+#num = range(1,10)
+
+#print(max(num))
+
+name = ["lile", "lile"]
+
+for name in name:
+    print(name)
+    (input("please enter your name: "))
+
+if name == "lile":
+    print("hello admin")
+else:
+    print("hello user")
+

8580494a204f48a5f07301ad8ba284b4f33c1590
lilecxvaradze <lilecxvaradze@gmail.com>
2024-07-28 20:54:47 +0100

    commit

---
+++ level33/homework/lesson33.py
@@ -0,0 +1,13 @@
+def find_short(s):
+    list1 = s.split(" ")
+
+
+    word_len = len(list1[0])
+    for i in list1:
+        if len(i) < word_len:
+            word_len = len(i)
+    
+
+    return word_len
+
+print(find_short("bitcoin take over the world maybe who knows perhaps"))
